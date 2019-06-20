55346140def0049d80b06682bfffbaaaae39e123
Kavya <skavya90@gmail.com>
2019-06-19 20:00:50 -0500

    Adding DataTable and related FLASK files

--- flask-ajax/flask-ajax/app.py
+++ flask-ajax/flask-ajax/app.py
@@ -28,16 +28,52 @@ app = Flask(__name__,template_folder='./templates')
 @app.route("/")
 def index():
      return render_template("index.html")
-@app.route("/<ZIPCODE>")
-def metadata(ZIPCODE):
-     ZipData = df.loc[df['neighbourhood'] == ZIPCODE]
-     return ZipData
+# @app.route("/<ZIPCODE>")
+# def metadata(ZIPCODE):
+#      ZipData = df.loc[df['neighbourhood'] == ZIPCODE]
+#      return ZipData
+@app.route("/zipcodes")
+def zips():
+    """Return a list of unique zipcodes."""
+    data = {
+         "zips": df.neighbourhood.unique().tolist()
+    }
+    return jsonify(data)
+
+
+@app.route("/zipcode/listings/<zipdata>")
+def zipcodelistings(zipdata):
+     # Zips = df['neighbourhood'].unique()
+     # return Zips
+     filtered_df = df[df.neighbourhood == int(zipdata)]
+     zip_df = filtered_df.groupby("room_type")["room_type"].count()
+     #price_df = filtered_df.groupby("room_type")["price"].mean()
+     jsondump = zip_df.to_json()
+     return jsondump
+
+@app.route("/zipcode/price/<zipcode>")
+def zipcodeprice(zipcode):
+     filtered_df = df[df.neighbourhood == (int(zipcode))]
+     price_df = round(filtered_df.groupby("room_type")["price"].mean(),2)
+     jsondump = price_df.to_json()
+     return jsondump
+
+@app.route("/zipchart/<zipdata>")
+def zipchart(zipdata):
+     filtered_df = df[df.neighbourhood == (int(zipdata))]
+     data = {
+        "Roomtype_Listings": filtered_df.groupby("room_type")["room_type"].count().to_json(),
+        "Roomtype_avgprice": round(filtered_df.groupby("room_type")["price"].mean(),2).to_json(),
+        "Roomtype_Reviews": filtered_df.groupby("room_type")["number_of_reviews"].sum().to_json(),
+     }
+     return jsonify(data)
+
 @app.route("/dtable")
 def table():
      return render_template("table.html")
 @app.route('/ajax')
 def hello_world(name=None):
      return df.to_json(orient='records')
-     
+
 if __name__ == '__main__':
     app.run()
--- flask-ajax/flask-ajax/static/js/logic.js
+++ flask-ajax/flask-ajax/static/js/logic.js
@@ -1,82 +1,146 @@
-function getdata(ZIPCODE) {
+function getdata(zipdata) {
 
     // @TODO: Complete the following function that builds the metadata panel
   
     // Use `d3.json` to fetch the metadata for a zipcode
-    var url = `/${ZIPCODE}`;
-    d3.json(url).then(function(zipdata){
+
+    d3.json(`/zipcode/listings/${zipdata}`).then(function(data){
       // Use d3 to select the panel with id of `#sample-metadata`
-      var zipcodes = d3.select("#metadata");
+      var zipcodes = d3.select("#zip_listings");
   
       // Use `.html("") to clear any existing metadata
-      metadata.html("");
-  
-  
+      zipcodes.html("");
       // Use `Object.entries` to add each key and value pair to the panel
       // Hint: Inside the loop, you will need to use d3 to append new
       // tags for each key-value in the metadata.
-      Object.entries(zipdata).forEach(function ([key, value]) {
-        var row = metadata.append("p");
+      Object.entries(data).forEach(function ([key, value]) {
+        var row = zipcodes.append("p");
         row.text(`${key}: ${value}`);
-  
   });
+    });
+
+    d3.json(`/zipcode/price/${zipdata}`).then(function(data){
+      // Use d3 to select the panel with id of `#sample-metadata`
+      var zip_price = d3.select("#zip_price");
+  
+      // Use `.html("") to clear any existing metadata
+      zip_price.html("");
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(data).forEach(function ([key, value]) {
+        var row = zip_price.append("p");
+        row.text(`${key}: ${value}`);
+  });
+    });
+    };
+  
+function buildCharts(sample) {
+  d3.json(`/zipchart/${zipdata}`).then(function(data){
+    var obj = data[Roomtype_Listings]
+    var keys = Object.keys(obj);
+    var Room_Types=[]
+    var Roomtype_values = []
+    var Roomtype_reviews = []
+    var Roomtype_prices = []
+      for (var i = 0; i < keys.length; i++) {
+        Room_Types.append(obj[keys[i]]);
+        if (i == 0){
+        Roomtype_values.append(data[Roomtype_Listings][obj[values[i]]]);
+        Roomtype_values.append(data[Roomtype_Reviews][obj[values[i]]]);
+        Roomtype_values.append(data[Roomtype_avgprice][obj[values[i]]]);
+        }
+
+        if (i == 1){
+          Roomtype_reviews.append(data[Roomtype_Listings][obj[values[i]]]);
+          Roomtype_reviews.append(data[Roomtype_Reviews][obj[values[i]]]);
+          Roomtype_reviews.append(data[Roomtype_avgprice][obj[values[i]]]);
+          }
+
+        if (i == 2){
+          Roomtype_prices.append(data[Roomtype_Listings][obj[values[i]]]);
+          Roomtype_prices.append(data[Roomtype_Reviews][obj[values[i]]]);
+          Roomtype_prices.append(data[Roomtype_avgprice][obj[values[i]]]);
+          }
+      // console.log(obj[keys[i]]);
+      // console.log()
+}
+console.log(Room_Types);
+console.log(Roomtype_values);
+console.log(Roomtype_reviews);
+  console.log(Roomtype_prices);
+  
+  
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: Room_Types,
+      datasets: [{
+        label: 'Listings count',
+        data: Roomtype_values
+      }, {
+        label: 'Average Price',
+        data: Roomtype_reviews
+      },{
+        label: 'Reviews Count',
+        data: Roomtype_prices
+      }]
     }
-  )};
+});
   
-  function buildCharts(sample) {
+  //   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  //   var url = `/samples/${sample}`;
+  //   d3.json(url).then(function(data) {
   
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-    var url = `/samples/${sample}`;
-    d3.json(url).then(function(data) {
+  //     // @TODO: Build a Bubble Chart using the sample data
+  //     var x_values = data.otu_ids;
+  //     var y_values = data.sample_values;
+  //     var m_size = data.sample_values;
+  //     var m_colors = data.otu_ids; 
+  //     var t_values = data.otu_labels;
   
-      // @TODO: Build a Bubble Chart using the sample data
-      var x_values = data.otu_ids;
-      var y_values = data.sample_values;
-      var m_size = data.sample_values;
-      var m_colors = data.otu_ids; 
-      var t_values = data.otu_labels;
-  
-      var trace1 = {
-        x: x_values,
-        y: y_values,
-        text: t_values,
-        mode: 'markers',
-        marker: {
-          color: m_colors,
-          size: m_size
-        } 
-      };
+  //     var trace1 = {
+  //       x: x_values,
+  //       y: y_values,
+  //       text: t_values,
+  //       mode: 'markers',
+  //       marker: {
+  //         color: m_colors,
+  //         size: m_size
+  //       } 
+  //     };
     
-      var data = [trace1];
+  //     var data = [trace1];
   
-      var layout = {
-        xaxis: { title: "OTU ID"},
-      };
+  //     var layout = {
+  //       xaxis: { title: "OTU ID"},
+  //     };
   
-      Plotly.newPlot('bubble', data, layout);
+  //     Plotly.newPlot('bubble', data, layout);
      
   
-      // @TODO: Build a Pie Chart
-      d3.json(url).then(function(data) {  
+  //     // @TODO: Build a Pie Chart
+  //     d3.json(url).then(function(data) {  
         
-      var pie_values = data.sample_values.slice(0,10);
-        var pie_labels = data.otu_ids.slice(0,10);
-        var pie_hover = data.otu_labels.slice(0,10);
+  //     var pie_values = data.sample_values.slice(0,10);
+  //       var pie_labels = data.otu_ids.slice(0,10);
+  //       var pie_hover = data.otu_labels.slice(0,10);
   
-        var data = [{
-          values: pie_values,
-          labels: pie_labels,
-          hovertext: pie_hover,
-          type: 'pie'
-        }];
+  //       var data = [{
+  //         values: pie_values,
+  //         labels: pie_labels,
+  //         hovertext: pie_hover,
+  //         type: 'pie'
+  //       }];
   
-        console.log(data);
+  //       console.log(data);
   
-        Plotly.newPlot('pie', data);
+  //       Plotly.newPlot('pie', data);
   
-      });
-    });   
-  }
+  //     });
+  //   });   
+  // }
   
   
   function init() {
@@ -84,28 +148,25 @@ function getdata(ZIPCODE) {
     var selector = d3.select("#selDataset");
   
     // Use the list of sample names to populate the select options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
+    d3.json("/zipcodes").then((zipcodes) => {
+      zipcodes["zips"].forEach((zipcode) => {
         selector
           .append("option")
-          .text(sample)
-          .property("value", sample);
+          .text(zipcode)
+          .property("value", zipcode);
       });
   
       // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
-      console.log("entering guage func")
-      buildGauge(firstSample);
+      const firstzip = zipcodes["zips"][0];
+      getdata(firstzip)
     });
   }
   
-  function optionChanged(newSample) {
+  function optionChanged(newZip) {
     // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-    buildGauge(newSample);
+  
+    getdata(newZip);
+    
   }
   
   // Initialize the dashboard

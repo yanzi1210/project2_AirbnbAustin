65e415594ed12c1f2660e4f883340917bb0e120d
Kavya <skavya90@gmail.com>
2019-06-22 02:11:54 -0500

    Uplaoding final Dashboard

--- flask-ajax/flask-ajax/static/js/Chartjs/logic.js
+++ flask-ajax/flask-ajax/static/js/Chartjs/logic.js
@@ -1,7 +1,5 @@
 function getdata(zipdata) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
   // Use `d3.json` to fetch the metadata for a zipcode
 
   d3.json(`/zipcode/listings/${zipdata}`).then(function (data) {
@@ -11,15 +9,11 @@ function getdata(zipdata) {
     // Use `.html("") to clear any existing metadata
     zipcodes.html("");
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
     Object.entries(data).forEach(function ([key, value]) {
       var row = zipcodes.append("p");
       row.text(`${key}: ${value}`);
     });
   });
-
-  //Fdosugfdsfa
 
   d3.json(`/zipcode/price/${zipdata}`).then(function (data) {
     // Use d3 to select the panel with id of `#sample-metadata`
@@ -28,8 +22,6 @@ function getdata(zipdata) {
     // Use `.html("") to clear any existing metadata
     zip_price.html("");
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
     Object.entries(data).forEach(function ([key, value]) {
       var row = zip_price.append("p");
       row.text(`${key}: $${value}`);
@@ -58,21 +50,6 @@ function buildCharts(zipdata) {
       Roomtype_values.push(obj[searchkey]);
       Roomtype_reviews.push(obj2[searchkey]);
       Roomtype_prices.push(obj3[searchkey]);
-
-
-      /* if (i == 1){
-         Roomtype_reviews.push(obj[searchkey]);
-         Roomtype_reviews.push(obj2[searchkey]);
-         Roomtype_reviews.push(obj3[searchkey]);
-         }
-
-       if (i == 2){
-         Roomtype_prices.push(obj[searchkey]);
-         Roomtype_prices.push(obj2[searchkey]);
-         Roomtype_prices.push(obj3[searchkey]);
-         } */
-
-
     }
 
     console.log(Room_Types);
@@ -102,60 +79,6 @@ function buildCharts(zipdata) {
     });
   });
 }
-
-//   // @TODO: Use `d3.json` to fetch the sample data for the plots
-//   var url = `/samples/${sample}`;
-//   d3.json(url).then(function(data) {
-
-//     // @TODO: Build a Bubble Chart using the sample data
-//     var x_values = data.otu_ids;
-//     var y_values = data.sample_values;
-//     var m_size = data.sample_values;
-//     var m_colors = data.otu_ids; 
-//     var t_values = data.otu_labels;
-
-//     var trace1 = {
-//       x: x_values,
-//       y: y_values,
-//       text: t_values,
-//       mode: 'markers',
-//       marker: {
-//         color: m_colors,
-//         size: m_size
-//       } 
-//     };
-
-//     var data = [trace1];
-
-//     var layout = {
-//       xaxis: { title: "OTU ID"},
-//     };
-
-//     Plotly.newPlot('bubble', data, layout);
-
-
-//     // @TODO: Build a Pie Chart
-//     d3.json(url).then(function(data) {  
-
-//     var pie_values = data.sample_values.slice(0,10);
-//       var pie_labels = data.otu_ids.slice(0,10);
-//       var pie_hover = data.otu_labels.slice(0,10);
-
-//       var data = [{
-//         values: pie_values,
-//         labels: pie_labels,
-//         hovertext: pie_hover,
-//         type: 'pie'
-//       }];
-
-//       console.log(data);
-
-//       Plotly.newPlot('pie', data);
-
-//     });
-//   });   
-// }
-
 
 function init() {
   // Grab a reference to the dropdown select element
